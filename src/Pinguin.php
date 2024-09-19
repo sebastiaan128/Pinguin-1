34b2f2079de29c077fad0498aa7e7025cd89cdce
Sebastiaan Balfoort <581150@edu.rocmn.nl>
2024-09-19 13:13:28 +0200

    chore: Update CSS styles and add font-family to body

--- src/Pinguin.php
+++ src/Pinguin.php
@@ -1,4 +1,18 @@
 <?php
- include 'navbar.html';
- ?>
- !
+include('navbar.html');
+?>
+
+<body>
+    <div class="flex justify-center items-center ">
+        <img class="w-full h-[500px] object-cover " src="IMG/home.png" alt="">
+            <p class="absolute flex items-center justify-center text-[#FFA502] text-[70px] lg:text-[175px] font-extralight text-shadow  ">Pinguin</p>
+    </div>
+    <div class="text  flex justify-center mt-[90px] mb-[90px]">
+        <p class="w-9/12 items-center">
+            Pinguïns, ook wel bekend als vetganzen, vormen een orde van niet-vliegende zeevogels die uitsluitend voorkomen op het zuidelijk halfrond. Ze behoren tot de orde Sphenisciformes, klasse Aves (vogels). Alle hedendaagse pinguïns behoren tot de familie Spheniscidae, maar er zijn uitgestorven soorten die buiten deze kroongroep vallen. Pinguïns zijn gemakkelijk te herkennen en hebben zich volledig aangepast aan extreme kou en het leven in zee, met een warm verenkleed als bescherming. <br><br>
+
+            Ze gebruiken hun vleugels om door het water te "vliegen," wat hen zeer efficiënte zwemmers maakt met snelheden tot wel 30 km per uur. De naam "pinguïn" komt waarschijnlijk van het Welsh pen gwyn, wat "witte kop" betekent, en werd oorspronkelijk gebruikt voor de reuzenalk, een inmiddels uitgestorven vogelsoort op het noordelijk halfrond, die geen nauwe verwant is van de pinguïn. Pinguïns leven voornamelijk in koude, ijzige omgevingen zoals Antarctica, waar ze door hun unieke lichaamsbouw en aanpassingen overleven. Ondanks dat ze niet kunnen vliegen, zijn pinguïns uitstekende zwemmers. <br><br>
+
+            Ze hebben zich meesterlijk aangepast aan het leven in zee, waar ze zich behendig door het water bewegen om voedsel, zoals vis en krill, te vangen. Hun zwarte rug en witte buik zorgen voor natuurlijke camouflage, waardoor ze minder opvallen voor roofdieren. Pinguïns staan ook bekend om hun sociale gedrag; ze vormen grote kolonies, waar ze samen broeden en elkaar beschermen tegen vijanden en extreme weersomstandigheden. Dankzij deze eigenschappen zijn pinguïns een van de meest geliefde en herkenbare dieren ter wereld.</p>
+    </div>
+</body>
