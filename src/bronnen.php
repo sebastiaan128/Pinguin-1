f380b5ee3313c37de26ada053e7bfefe7178bc4d
Kevinz1708 <128469729+Kevinz1708@users.noreply.github.com>
2024-09-25 11:33:49 +0200

    Update bronnen.php to add body tag and fix formatting

--- src/bronnen.php
+++ src/bronnen.php
@@ -1,35 +1,87 @@
 <?php
 include('navbar.html');
 ?>
+
 <body>
-    
+    <div class="py-24 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:mx-0">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Bronnen</h2>
+                <p class="mt-2 text-lg leading-8 text-gray-600"></p>
+            </div>
 
-<div class=" py-24 sm:py-32">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl lg:mx-0">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Bronnen</h2>
-            <p class="mt-2 text-lg leading-8 text-gray-600"></p>
-        </div>
+            <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+                <article class="flex max-w-xl flex-col items-start justify-between">
+                    <div class="group relative">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                ChatGPT
+                            </a>
+                        </h3>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"></p>
+                    </div>
 
-        <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            <article class="flex max-w-xl flex-col items-start justify-between">
-                <div class="group relative">
-                    <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                        <a href="#">
-                            <span class="absolute inset-0"></span>
-                            Boost your conversion rate
-                        </a>
-                    </h3>
-                    <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                </div>
-                <br>
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
+                    <img src="https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo.jpg" alt="" class="h-10 w-10 rounded-full bg-gray-50 object-cover">
 
-            </article>
+                    <br>
+                    <a href="https://openai.com/chatgpt/" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
+                </article>
 
+                <article class="flex max-w-xl flex-col items-start justify-between">
+                    <div class="group relative">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                Beekse Bergen
+                            </a>
+                        </h3>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"></p>
+                    </div>
+
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_Ft2d1-tZE5Syhd1C_Xi_vyCq0zpGbPfdWQ&s" alt="" class="h-10 w-10 rounded-full bg-gray-50 object-cover">
+
+                    <br>
+                    <a href="https://www.beeksebergen.nl/op-safari/dieren/pinguin" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
+                </article>
+
+                <article class="flex max-w-xl flex-col items-start justify-between">
+                    <div class="group relative">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                Klokhuis
+                            </a>
+                        </h3>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"></p>
+                    </div>
+
+                    <img src="https://cbsmozaiek.nl/wp-content/uploads/2021/03/klokhuis.jpg" alt="" class="h-10 w-10 rounded-full bg-gray-50 object-cover">
+
+                    <br>
+                    <a href="https://hetklokhuis.nl/onderwerp/pinguin" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
+                </article>
+
+                <article class="flex max-w-xl flex-col items-start justify-between">
+                    <div class="group relative">
+                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                Youtube
+                            </a>
+                        </h3>
+                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"></p>
+                    </div>
+
+                    <img src="https://static.vecteezy.com/system/resources/previews/023/986/480/original/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png" alt="" class="h-10 w-10 rounded-full bg-gray-50 object-cover">
+
+                    <br>
+                    <a href="https://www.youtube.com/results?search_query=pinguin" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Read more</a>
+                </article>
+
+            </div>
         </div>
     </div>
-</div>
 </body>
 
 <?php
